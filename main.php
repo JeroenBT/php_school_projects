edc570b224a9f261f1c25d77f13ee420bfc3ff07
Joengz <jns-b@hotmail.com>
2016-02-16 12:33:42 +0100

    New update

--- main.php
+++ main.php
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
- <ul>
+<ul>
+  <li><a href="foutmelding.php">Foutmelding</a></li>
   <li><a href="busreis.php">Busreis</a></li>
   <li><a href="kerstboom.php">Kerstboom</a></li>
-</ul> 
+  <li><a href="while-naar-for-loop.php">While naar For loop</a></li>
+  <li><a href="zwemclubs.php">Zwemclubs</a></li>
+  <li><a href="kapperszaak.php">Kapperszaak Sanders</a></li>
+</ul>
 
 <?php
     echo 'Hello world!';
-=======
-<form name="form" id="leeftijd" method="POST">
-<input type="int" name="leeftijd" id="leeftijd" class="int" maxlength="2">
-</form>
-
-<?php
-	$get_leeftijd = $_POST["leeftijd"];
-	$prijs = 10;
-	$korting;
-	if ($get_leeftijd >= 65) {
-	$korting = $prijs * 0.5;
-	} else if ($get_leeftijd <= 3) {
-	$korting = $prijs * 0;
-	} else if ($get_leeftijd <= 12) {
-	$korting = $prijs * 0.5;
-	} else {
-	$korting = $prijs * 1;
-	}
-    echo nl2br('Leeftijd: '.$get_leeftijd.' <br> Prijs: '.$korting);
->>>>>>> 6ae2a72751d95f6e3826f512e13e0a0e5e6b026a
