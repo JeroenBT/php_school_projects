1c26e52dd4f5f438d2b5af50ce2657eb551ae16e
Joengz <jns-b@hotmail.com>
2016-02-16 12:31:49 +0100

    Create zwemclubs.php
    New excercise

---
+++ zwemclubs.php
@@ -0,0 +1,17 @@
+<?php
+	$zwemclub['De spartelkuikens'] = "15";
+	$zwemclub['De waterbuffels'] = "32";
+	$zwemclub['Plonsmderin'] = "11";
+	$zwemclub['Bommetje'] = "23";
+	foreach($zwemclub as $index => $value) {
+		$waarde = $value;
+		if ($value = intval($waarde /5)) {
+		echo "Zwemclub: " . $index . ", Leden aantal: " . $waarde;
+		echo "<br>";
+			for ($i = 0; $i < $value; $i++) {
+				echo "X ";
+			}
+			echo "<br>";
+		}
+	}
+?> 
