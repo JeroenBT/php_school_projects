942aab827b2a3855c9808c231bc03188e45f0cb3
Joengz <jns-b@hotmail.com>
2016-04-06 09:27:55 +0200

    Edited main.php

--- main.php
+++ main.php
@@ -5,6 +5,12 @@
   <li><a href="while-naar-for-loop.php">While naar For loop</a></li>
   <li><a href="zwemclubs.php">Zwemclubs</a></li>
   <li><a href="kapperszaak.php">Kapperszaak Sanders</a></li>
+  <li><a href="functions.php">Dergelijke Functies</a></li>
+  <li><a href="dynamische-website.php">Dynamische Website</a></li>
+  <li><a href="autos.php">Autolijsten</a></li>
+  <li><a href="loginandadmin.php">Dynamische Website 2 (Login and Admin)</a></li>
+  <li><a href="radiozender.php">Radiozender</a></li>
+  <li><a href="aapoverzicht.php">Apen Overzicht</a></li>
 </ul>
 
 <?php
